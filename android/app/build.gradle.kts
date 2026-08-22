005a0702a0d41484185bb2ab5ff3969a0d2f040d
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-08-22 07:49:22 +0000

    fix: align Kotlin and Java JVM targets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,15 @@ android {
     }
 
     buildFeatures { compose = true }
-    kotlinOptions { jvmTarget = "17" }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
 
 dependencies {
