20ccde451f7a928e3dc07f7e15b556748fe3ced3
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-08-22 06:52:03 +0000

    feat(android): add configurable signaling endpoint

---
+++ android/app/src/main/java/com/linkshare/app/data/SignalingConfig.kt
@@ -0,0 +1,6 @@
+package com.linkshare.app.data
+
+object SignalingConfig {
+    /** Set this to the deployed HTTPS LINKO signaling URL before release. */
+    const val BASE_URL: String = "https://REPLACE_WITH_LINKO_SIGNALING_HOST"
+}
