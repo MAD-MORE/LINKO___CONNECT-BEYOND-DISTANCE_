2790505543fc5d4650a63b518da01f9d3dbc60b4
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-08-22 14:40:04 +0000

    Add prototype LINKO color system

---
+++ android/app/src/main/java/com/linkshare/app/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.linkshare.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BG      = Color(0xFF080808)
+val Surface = Color(0xFF111111)
+val Card    = Color(0xFF181818)
+val Card2   = Color(0xFF1E1E1E)
+val Border  = Color(0xFF242424)
+
+val Blue    = Color(0xFF3B7EF6)
+val BlueD   = Color(0xFF1A56C4)
+val Green   = Color(0xFF22C55E)
+val Yellow  = Color(0xFFF59E0B)
+val Red     = Color(0xFFEF4444)
+
+val TextPrimary   = Color(0xFFF2F2F2)
+val TextSub       = Color(0xFFA0A0A0)
+val TextMuted     = Color(0xFF505050)
