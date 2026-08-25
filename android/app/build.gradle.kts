19ac398d02cc9ae7c1246833a868452113927a71
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-08-25 07:32:00 +0000

    fix: align Kotlin stdlib with compiler

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 
 configurations.all {
     resolutionStrategy {
+        force("org.jetbrains.kotlin:kotlin-stdlib:2.2.21")
         force("androidx.core:core:1.16.0")
         force("androidx.core:core-ktx:1.16.0")
     }
